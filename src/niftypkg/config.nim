8a3533ce61bde19766767219dfc8cd564118ac44
h3rald <h3rald@h3rald.com>
2023-08-02 15:17:50 +0000

    Updated release.

--- src/niftypkg/config.nim
+++ src/niftypkg/config.nim
@@ -1,5 +1,5 @@
 const
   pkgTitle*       = "nifty"
-  pkgVersion*     = "1.2.2"
+  pkgVersion*     = "1.2.3"
   pkgAuthor*      = "Fabio Cevasco"
   pkgDescription* = "A decentralized (pseudo) package manager and script runner."
